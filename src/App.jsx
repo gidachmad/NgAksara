9c9cf335179732de0586f3b9c6edae6fe914d323
Gid AAF <gaaf0809@gmail.com>
2023-01-12 23:13:44 +0700

    fixing images, setting up dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
-import Dashboard from './layouts/Dashboard'
+import { useState } from 'react'
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 import { Outlet } from 'react-router-dom'
 import { ScrollToTop } from './utils/ScrollToTop'
 
 export default function App() {
+  const [lightTheme, setLightTheme] = useState(true)
+
+  const changeTheme = () => {
+    setLightTheme(!lightTheme)
+  }
+
   ScrollToTop()
   return (
-    <>
-      <div className='flex flex-col min-h-screen text-coffee-900'>
-        <div className='flex-grow flex-shrink-0 basis-auto'>
-          <Navbar />
+    <div className={`${lightTheme ? '' : 'dark'}`}>
+      <div className='flex flex-col min-h-screen text-coffee-900 dark:text-coffee-500 '>
+        <div className='flex-grow flex-shrink-0 basis-auto dark:bg-gray-900 transition-all duration-300'>
+          <Navbar changeTheme={changeTheme} lightTheme={lightTheme} />
           <main className='pt-8 '>
             <Outlet />
           </main>
         </div>
         <Footer />
       </div>
-    </>
+    </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,15 +4,11 @@ import { Link } from 'react-router-dom'
 import ngaksaraLogo from '../assets/ngaksara-logo.png'
 import { route } from '../routes/Routes'
 
-export default function Navbar() {
-  const [isLightTheme, setIsLightTheme] = useState(true)
-
-  const changeTheme = () => {
-    setIsLightTheme(!isLightTheme)
-  }
-
+export default function Navbar({ changeTheme, lightTheme }) {
   return (
-    <header id='navbar' className='border-b-2 sticky top-0 bg-white'>
+    <header
+      id='navbar'
+      className='border-b-2 sticky top-0 bg-white dark:bg-gray-900 duration-300'>
       <div className='flex w-5/6 py-6 mx-auto items-center'>
         <Link to='/'>
           <img src={ngaksaraLogo} alt='' className='max-h-16' />
@@ -29,21 +25,21 @@ export default function Navbar() {
           </ul>
         </nav>
         {/* <MoonIcon className='w-6 h-6' /> */}
-        {/* <button
-          className='ml-6 group relative border-2 border-coffee-900 hover:border-coffee-300 rounded-full p-1 hover-transition 
+        <button
+          className='ml-6 group relative border-2 hover:border-coffee-900 border-coffee-300 rounded-full p-1 hover-transition 
           '
-          onClick={changeTheme}>
-          {isLightTheme ? (
-            <MoonIcon className='w-6 h-6 fill-coffee-900 group-hover:fill-coffee-300 dark:fill-coffee-200 hover-transition ' />
+          onClick={() => changeTheme()}>
+          {lightTheme ? (
+            <MoonIcon className='w-6 h-6 fill-coffee-900 group-hover:fill-coffee-900 dark:fill-coffee-300 hover-transition ' />
           ) : (
-            <SunIcon className='w-6 h-6 fill-coffee-900 group-hover:fill-coffee-300 dark:fill-coffee-200 hover-transition ' />
+            <SunIcon className='w-6 h-6 fill-coffee-900 group-hover:fill-coffee-900 dark:fill-coffee-300 hover-transition ' />
           )}
           <span
             className='absolute pointer-events-none hidden group-hover:flex min-w-max left-1/2 -translate-x-1/2 -bottom-10
             px-2 py-1 bg-coffee-700 rounded-lg text-center text-white text-sm  '>
-            Berubah ke mode {isLightTheme ? 'gelap' : 'terang'}
+            Berubah ke mode {lightTheme ? 'gelap' : 'terang'}
           </span>
-        </button> */}
+        </button>
       </div>
     </header>
   )
