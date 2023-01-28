6d28d29ec6c102654144a5063bbc39c19ee06a6f
Gid AAF <gaaf0809@gmail.com>
2023-01-28 22:55:40 +0700

    fixing stuff

--- src/utils/utils.jsx
+++ src/utils/utils.jsx
@@ -1,14 +1,11 @@
 export const getImage = (name) => {
-  const path = new URL(`../assets/dashboard/${name}.svg`, import.meta.url).href
-  return path
+  return new URL(`../assets/dashboard/${name}.svg`, import.meta.url).href
 }
 
 export const getThumbnailKelas = (name) => {
-  const path = new URL(`../assets/Thumbnail/${name}.png`, import.meta.url).href
-  return path
+  return new URL(`../assets/Thumbnail/${name}.png`, import.meta.url).href
 }
 
 export const getKelasAksara = (kelas, name) => {
-  const path = new URL(`../assets/${kelas}/${name}`, import.meta.url).href
-  return path
+  return new URL(`../assets/${kelas}/${name}.png`, import.meta.url).href
 }
